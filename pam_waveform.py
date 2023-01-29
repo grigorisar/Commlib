afd4627cd5fbb1f848ba4e48d1a89b92d921e666
Salocip <impolitebagels@gmail.com>
2023-01-29 16:48:32 +0200

    Added more code to pam_waveform.py

--- pam_waveform.py
+++ pam_waveform.py
@@ -1,6 +1,8 @@
 import numpy as np
 from kam import pam_constellation
 from scipy import special
+import textwrap as tw
+
 
 def toBinary(a):
   l,m=[],[]
@@ -25,10 +27,76 @@ def gray_code(m):
     return g
 
 name = "Grigoris Arfanis"
-print (toBinary(name))
-print (len(gray_code(8)))
+# print (toBinary(name))
+# print (len(gray_code(8)))
 
 pam = pam_constellation(M=2,title="This is a test")
 pam.set_symbols(toBinary(name))
 pam.set_gray_bits(m=4)
 pam.plot()
+
+
+
+
+name = input("Enter name:").title()
+surname = input("Enter surname:").title()
+chars = list(name + surname) 
+
+# print(string2bin(chars))
+
+# print(chars)
+
+ords = [ord(character) for character in chars]
+# print([bin(b).replace('b', '', 1)  for b in ords])
+
+binary_list = [bin(b).replace('b', '', 1)  for b in ords]
+
+binary = "".join(binary_list)
+
+bin2 = tw.wrap(binary, 1)
+
+bin2 = [int(char) for char in bin2]
+print(bin2)
+
+gray2 = [0] * len(bin2)  
+# iterate through each bit ignoring first one applying xor, current new bit = old previous bit XOR old current bit
+for idx in range(1, len(bin2)):
+    prev = bin2[idx - 1]
+    # print(f'prev = '+ str(prev))
+    curr = bin2[idx]
+    # print(f'curr = '+ str(prev))
+
+    if prev + curr == 1 :
+        gray2[idx] = 1
+    else:
+        gray2[idx] = 0
+        
+    # print(f'new for pos ' + str(idx) + ' = '+ str(bin2[char]))
+
+print(gray2)
+
+length = 7
+
+bin2 = [str(char) for char in bin2]
+bin3 = []
+
+for i in range(1, length):
+    first_half = bin2.copy()
+    sec_half = bin2.copy()
+    first_half = ['0' + bit for bit in first_half]
+    sec_half = ['1' + bit for bit in sec_half] 
+    bin2 = first_half + sec_half
+print(bin2)
+
+
+
+# print(binary + str(len(binary)))
+# print(binaryin2)
+
+# if len(binary) % 3 != 0 :
+#     pad_times = (len(binary) % 3) 
+#     pad = ''
+#     for i in range(1, pad_times):
+#         pad = pad + '0'
+
+#     binary = binary + pad
